15c30f1bea45c1f1147bcfe7be5cece6c4497ab7
Amaravenisneha <104358022+Amaravenisneha@users.noreply.github.com>
2022-05-01 19:10:31 +0530

    This is a new file

---
+++ File1.sh
@@ -0,0 +1,7 @@
+On branch Sneha
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	.File1.sh.swp
+	File1.sh
+
+nothing added to commit but untracked files present (use "git add" to track)
