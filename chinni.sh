699a05433abde68742314d677a1dc4bce2f98eaf
Amaravenisneha <104358022+Amaravenisneha@users.noreply.github.com>
2022-05-02 12:30:01 +0530

    Update chinni.sh

--- chinni.sh
+++ chinni.sh
@@ -1,7 +1,1 @@
-On branch jaya
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-	.chinni.sh.swp
-	chinni.sh
-
-nothing added to commit but untracked files present (use "git add" to track)
+echo "This is the second file created"
