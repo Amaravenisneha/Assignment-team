a1a24f0d66b519de935d09b341f402913514f959
jaya-git-sre <104379918+jaya-git-sre@users.noreply.github.com>
2022-05-02 12:04:12 +0530

    this is my last update

---
+++ chinni.sh
@@ -0,0 +1,7 @@
+On branch jaya
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	.chinni.sh.swp
+	chinni.sh
+
+nothing added to commit but untracked files present (use "git add" to track)
